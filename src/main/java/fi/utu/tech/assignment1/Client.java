8f5aee855a3ad78ce3cede2a33c827da577df2a5
SipuliCodes <vepsalainensisu@gmail.com>
2024-11-18 11:31:22 +0200

    tehtävä 1 tehty

--- src/main/java/fi/utu/tech/assignment1/Client.java
+++ src/main/java/fi/utu/tech/assignment1/Client.java
@@ -1,9 +1,17 @@
 package fi.utu.tech.assignment1;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
     public static void main(String[] args) {
-        // TODO: Asiakasohjelma
+        try(Socket s = new Socket("localhost", 18112);) {
+            System.out.println("Connected");
+        } catch (IOException | SecurityException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/fi/utu/tech/assignment1/Server.java
+++ src/main/java/fi/utu/tech/assignment1/Server.java
@@ -1,9 +1,19 @@
 package fi.utu.tech.assignment1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args) {
-        // TODO: Palvelinohjelma
+        try(ServerSocket serverSocket = new ServerSocket(18112);) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Asiakas yhdistänyt");
+            socket.close();
+        } catch (IOException | IllegalArgumentException | SecurityException e) {
+            e.printStackTrace();
+        }
     }
     
 }
