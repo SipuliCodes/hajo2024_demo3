94b2c2136bea44f2b8665b4966c6fb9ceffd429d
SipuliCodes <vepsalainensisu@gmail.com>
2024-11-18 12:52:25 +0200

    tehtävä 2 tehty

--- src/main/java/fi/utu/tech/assignment2/Server.java
+++ src/main/java/fi/utu/tech/assignment2/Server.java
@@ -1,9 +1,22 @@
 package fi.utu.tech.assignment2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+        try(ServerSocket serverSocket = new ServerSocket(18112);) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Asiakas yhdistänyt");
+            InputStream is = socket.getInputStream();
+            System.out.println("Asiakas sanoo: " + new String(is.readAllBytes()));
+            socket.close();
+        } catch (IOException | IllegalArgumentException | SecurityException e) {
+            e.printStackTrace();
+        }
     }
 
 }
